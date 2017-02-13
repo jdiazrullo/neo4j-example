c69603da86bc62e82e27411e31f14578fa62ccb4
Jaime Diaz Rullo <jaime.diaz-rullo@innovation4security.com>
2017-02-13 14:17:12 +0100

    project structure

---
+++ src/main/java/com/example/DemoNeo4jApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNeo4jApplication.class, args);
+	}
+}
